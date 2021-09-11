c201d673b0fb88df9b93ec892e6091550540c4a8
alexisjrossi <alexisjrossi@gmail.com>
2021-09-11 00:07:18 -0300

    add expenses components

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,13 +1,18 @@
-function ExpenseItem() {
-    return (
-        <div>
-            <div>Date</div>
-            <div>
-                <div>Title</div>
-                <div>Amount</div>
-            </div>
-        </div>
-    );
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>
+        <ExpenseDate date={props.date} />
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
